ba09e1640b7f173ca80ed62200b7602313734e7f
Marinos <mhadjimitsis@gmail.com>
2026-03-08 23:09:06 +0200

    map marker fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useEffect, useMemo, useState } from "react";
 import { supabase } from "./lib/supabase";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 const categories = [
   "Καθαριότητα / Σκουπίδια",
